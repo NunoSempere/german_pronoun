d21fa610ba1610242f54b4e31b7d9684b2f96ae9
NunoSempere <nuno.sempere@gmail.com>
2021-09-17 17:46:44 +0200

    feat: Updated to use search
    If one goes directly to the guessed url, it fails sometimes
    e.g., with Boot

--- german_pronoun.sh
+++ german_pronoun.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 function de(){
-	dudenURL="https://www.duden.de/rechtschreibung/$1"
-	fileName="$1.temp"
-	wget "$dudenURL" --quiet -O "$fileName"
-	grep -A 1 Wortart "$fileName" | tail -1 | sed 's/.*">//' | sed 's/<.*//' 
-	rm "$fileName"
+        fileName="$1.temp"
+        dudenURL="https://www.duden.de/suchen/dudenonline/$1"
+        wget "$dudenURL" --quiet -O "$fileName"
+        answer="$(grep "strong" "$fileName" | head -2 | tail -1 | sed -E "s|<(/)?strong>||g" | sed "s|^\s*||g")"
+        attributes="$(grep -A 3 vignette__snippet "$fileName" | head -4 | tail -1 | sed 's/–.*$//g' | sed "s|^\s*||g")"
+        echo "$answer: $attributes"
+        rm "$fileName"
 }
 
 de $1
